1f779c4b018f8544a09b8bbb0f30ff1205aab332
Mystic <mystic.rick@gmail.com>
2010-09-29 15:06:34 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RQuoteIT::Application.config.secret_token = '228dcf226b8359073cd603c68ca54b4ab8a18cd8b97c807085a1c7400b7b880575697232e22b58bfd233b3bcd85ad860ca0c284ca10d066441507ff63dca3c81'
